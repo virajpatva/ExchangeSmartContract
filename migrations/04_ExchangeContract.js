cf8b66f988bb636d9ae908a901bfba99b9ca8cfd
virajpatva <viraj.patva@smartsensesolutions.com>
2022-10-19 15:22:14 +0530

    updated migration script

--- migrations/04_ExchangeContract.js
+++ migrations/04_ExchangeContract.js
@@ -1,8 +1,9 @@
 const ExchangeContract = artifacts.require('ExchangeContract');
 const USDT = artifacts.require('USDT');
+const wBTC = artifacts.require('WrappedBTC');
 const PriceFeedContract = artifacts.require("PriceConsumerV3");
 const { NetworkConfig } = require('../helper-truffle-config.js');
 const chainID = config.network_id;
 module.exports = function (deployer){
-    deployer.deploy(ExchangeContract,PriceFeedContract.address,USDT.address,NetworkConfig[chainID].ETH_BTC);
+    deployer.deploy(ExchangeContract,PriceFeedContract.address,USDT.address,wBTC.address,NetworkConfig[chainID].ETH_BTC);
 }
