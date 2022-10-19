8df9a413bd4fc1f2cf26408610e7a54cc5e2590d
virajpatva <viraj.patva@smartsensesolutions.com>
2022-10-19 10:03:06 +0530

    Unit Testing with Truffle Assert

--- contracts/ExchangeContract.sol
+++ contracts/ExchangeContract.sol
@@ -2,16 +2,16 @@
 pragma solidity ^0.8.7;
 import "./PriceFeedContract.sol";
 import "./USDTContract.sol";
-
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 contract ExchangeContract{ 
 
     PriceConsumerV3 PriceFeedInstance;
-    USDT USDTContractInstance;
+    IERC20 USDTContractInstance;
     address owner ; 
 
     constructor(address PriceFeedAddress,address USDTContractAddress){
         PriceFeedInstance = PriceConsumerV3(PriceFeedAddress);      
-        USDTContractInstance = USDT(USDTContractAddress);
+        USDTContractInstance = IERC20(USDTContractAddress);
         owner = msg.sender;
     }
 
@@ -21,8 +21,8 @@ contract ExchangeContract{
     }
 
     function USDTtoETH (uint EthQty) public payable{
-        require(address(this).balance >= EthQty,"Invalid Quantity");
-        uint latestPrice= uint(PriceFeedInstance.getLatestPrice());
+        require(address(this).balance >= EthQty * 10 ** 18,"Invalid Quantity");
+        uint latestPrice = uint(PriceFeedInstance.getLatestPrice());
         uint USDTQty = latestPrice * EthQty;
         USDTContractInstance.transferFrom(msg.sender,address(this),USDTQty);
         payable (msg.sender).transfer(EthQty * 10 ** 18);
--- helper-truffle-config.js
+++ helper-truffle-config.js
@@ -2,7 +2,9 @@ const NetworkConfig = {
     "1" : {
         name: "main",
         ETH_USD :  "0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419",  
-        USDT_USD : "0x3E7d1eAB13ad0104d2750B8863b489D65364e32D",    
+        USDT_USD : "0x3e7d1eab13ad0104d2750b8863b489d65364e32d",   
+        BTC_ETH : "0xdeb288f737066589598e9214e782fa5a8ed689e8" 
+        ETH_BTC : "0xAc559F25B1619171CbC396a50854A3240b6A4e99"
     }
 }
 
