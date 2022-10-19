15bd33aaf859e7b9efc085ae4056601fcf6482f3
virajpatva <viraj.patva@smartsensesolutions.com>
2022-10-19 15:17:45 +0530

    wBtc Exchange added

--- migrations/03_ExchangeContract.js
+++ migrations/03_ExchangeContract.js
@@ -1,6 +1,8 @@
 const ExchangeContract = artifacts.require('ExchangeContract');
 const USDT = artifacts.require('USDT');
 const PriceFeedContract = artifacts.require("PriceConsumerV3");
+const { NetworkConfig } = require('../helper-truffle-config.js');
+const chainID = config.network_id;
 module.exports = function (deployer){
-    deployer.deploy(ExchangeContract,PriceFeedContract.address,USDT.address);
+    deployer.deploy(ExchangeContract,PriceFeedContract.address,USDT.address,NetworkConfig[chainID].ETH_BTC);
 }
